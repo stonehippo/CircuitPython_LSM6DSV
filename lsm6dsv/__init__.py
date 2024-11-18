efa4488148717afa4470e3b110c433df5b3c5e47
George White <stonehippo@gmail.com>
2024-11-18 00:33:35 -0500

    Basic accel and gyro reading

--- lsm6dsv/__init__.py
+++ lsm6dsv/__init__.py
@@ -77,6 +77,8 @@ _LSM6DSV_WHOAMI = const(0xF)
 _LSM6DSV_CTRL1 = const(0x10) # accelerometer control register
 _LSM6DSV_CTRL2 = const(0x11) # gyroscope control register
 _LSM6DSV_CTRL3 = const(0x12) # control register
+_LSM6DSV_CTRL6 = const(0x15) # control register; includes gyro full scale
+_LSM6DSV_CTRL8 = const(0x17) # control register; includes accel full scale
 _LSM6DSV_OUT_TEMP_L = const(0x20)
 _LSM6DSV_OUTX_L_G = const(0x22)
 _LSM6DSV_OUTX_L_A = const(0x28)
@@ -96,8 +98,10 @@ class LSM6DSV_CORE:
 
     #RWBits
     _accel_data_rate = RWBits(4, _LSM6DSV_CTRL1, 0)
+    _accel_range = RWBits(2, _LSM6DSV_CTRL8, 0)
 
     _gyro_data_rate = RWBits(4, _LSM6DSV_CTRL2, 0)
+    _gyro_range = RWBits(4, _LSM6DSV_CTRL6, 0)
 
     _sw_reset = RWBit(_LSM6DSV_CTRL3, 0)
     _bdu = RWBit(_LSM6DSV_CTRL3, 6)
@@ -118,8 +122,10 @@ class LSM6DSV_CORE:
             )
         self.reset()
 
-        sleep(0.1)
-
+        self._add_gyro_ranges()
+        self._add_accel_ranges()
+        
+        # Default data rates for both accel and gyro is 0 (off); enable both at lowest standard rate (non-low-power)
         self.accelerometer_data_rate = Rate.RATE_120_HZ  # pylint: disable=no-member
         self.gyro_data_rate = Rate.RATE_120_HZ  # pylint: disable=no-member
 
@@ -133,6 +139,88 @@ class LSM6DSV_CORE:
         while self._sw_reset:
             sleep(0.001)
 
+    @staticmethod
+    def _add_gyro_ranges() -> None:
+        GyroRange.add_values(
+            (
+                ("RANGE_125_DPS", 0, 125, 4.375),
+                ("RANGE_250_DPS", 1, 250, 8.75),
+                ("RANGE_500_DPS", 2, 500, 17.50),
+                ("RANGE_1000_DPS", 3, 1000, 35.0),
+                ("RANGE_2000_DPS", 4, 2000, 70.0),
+                ("RANGE_4000_DPS", 12, 4000, 140.0),
+            )
+        )
+
+    @staticmethod
+    def _add_accel_ranges() -> None:
+        AccelRange.add_values(
+            (
+                ("RANGE_2G", 0, 2, 0.061),
+                ("RANGE_4G", 1, 4, 0.122),
+                ("RANGE_8G", 2, 8, 0.244),
+                ("RANGE_16G", 3, 16, 0.488),
+            )
+        )
+
+    @property
+    def acceleration(self) -> Tuple[float, float, float]:
+        """The x, y, z acceleration values returned in a 3-tuple and are in m / s ^ 2."""
+        raw_accel_data = self._raw_accel_data
+
+        x = self._scale_xl_data(raw_accel_data[0])
+        y = self._scale_xl_data(raw_accel_data[1])
+        z = self._scale_xl_data(raw_accel_data[2])
+
+        return (x, y, z)
+
+    @property
+    def gyro(self) -> Tuple[float, float, float]:
+        """The x, y, z angular velocity values returned in a 3-tuple and are in radians / second"""
+        raw_gyro_data = self._raw_gyro_data
+        x, y, z = [radians(self._scale_gyro_data(i)) for i in raw_gyro_data]
+        return (x, y, z)
+
+    def _scale_xl_data(self, raw_measurement: int) -> float:
+        return (
+            raw_measurement
+            * AccelRange.lsb[self._cached_accel_range]
+            * _MILLI_G_TO_ACCEL
+        )
+
+    def _scale_gyro_data(self, raw_measurement: int) -> float:
+        return raw_measurement * GyroRange.lsb[self._cached_gyro_range] / 1000
+    
+    @property
+    def accelerometer_range(self) -> int:
+        """Adjusts the range of values that the sensor can measure, from +/- 2G to +/-16G (or +/- 4G to +/- 32G for some chips)
+        Note that larger ranges will be less accurate. Must be an ``AccelRange``"""
+        return self._cached_accel_range
+
+    # pylint: disable=no-member
+    @accelerometer_range.setter
+    def accelerometer_range(self, value: int) -> None:
+        if not AccelRange.is_valid(value):
+            raise AttributeError("range must be an `AccelRange`")
+        self._accel_range = value
+        self._cached_accel_range = value
+        sleep(0.2)  # needed to let new range settle
+
+    @property
+    def gyro_range(self) -> int:
+        """Adjusts the range of values that the sensor can measure, from 125 Degrees/s to 4000
+        degrees/s. Note that larger ranges will be less accurate. Must be a ``GyroRange``.
+        """
+        return self._cached_gyro_range
+
+    @gyro_range.setter
+    def gyro_range(self, value: int) -> None:
+        if not GyroRange.is_valid(value):
+            raise AttributeError("range must be a `GyroRange`")
+        self._gyro_range = value
+        self._cached_gyro_range = value
+        sleep(0.2) # needed to let new range settle
+
     @property
     def accelerometer_data_rate(self) -> int:
         """Select the rate at which the accelerometer takes measurements. Must be a ``Rate``"""
@@ -140,8 +228,8 @@ class LSM6DSV_CORE:
 
     @accelerometer_data_rate.setter
     def accelerometer_data_rate(self, value: int) -> None:
-        # if not Rate.is_valid(value):
-        #     raise AttributeError("accelerometer_data_rate must be a `Rate`")
+        if not Rate.is_valid(value):
+            raise AttributeError("accelerometer_data_rate must be a `Rate`")
 
         self._accel_data_rate = value
 
@@ -152,8 +240,8 @@ class LSM6DSV_CORE:
 
     @gyro_data_rate.setter
     def gyro_data_rate(self, value: int) -> None:
-        # if not Rate.is_valid(value):
-        #     raise AttributeError("gyro_data_rate must be a `Rate`")
+        if not Rate.is_valid(value):
+            raise AttributeError("gyro_data_rate must be a `Rate`")
 
         self._gyro_data_rate = value
 
--- lsm6dsv/lsm6dsv32x.py
+++ lsm6dsv/lsm6dsv32x.py
@@ -1,4 +1,25 @@
-from . import LSM6DSV_CORE
+from . import LSM6DSV_CORE, LSM6DSV_DEFAULT_ADDRESS, AccelRange
+
+try:
+    import typing  # pylint: disable=unused-import
+    from busio import I2C
+except ImportError:
+    pass
 
 class LSM6DSV32X(LSM6DSV_CORE):
-	CHIP_ID = 0x70
+    CHIP_ID = 0x70
+
+    def __init__(self, i2c_bus: I2C, address: int = LSM6DSV_DEFAULT_ADDRESS) -> None:
+        super().__init__(i2c_bus, address)
+        self.accelerometer_range = AccelRange.RANGE_8G  # pylint:disable=no-member
+
+    @staticmethod
+    def _add_accel_ranges() -> None:
+        AccelRange.add_values(
+            (
+                ("RANGE_4G", 0, 2, 0.122),
+                ("RANGE_8G", 1, 4, 0.244),
+                ("RANGE_16G", 2, 8, 0.488),
+                ("RANGE_32G", 3, 16, 0.976),
+            )
+        )
